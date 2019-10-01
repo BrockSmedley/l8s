4515d8ceb2da3911336dd016218104062b6077bb
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-09-30 22:16:55 -0700

    add fixed navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () =>
+    <nav class="uk-navbar-container navbar" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><a href="#">Home</a></li>
+                <li class="uk-parent"><a href="#">Other</a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
+    </nav>

13abdc27b52c01d925e93b2977d9cd7b788c10b4
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-10-06 01:13:14 -0700

    neato modal details

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
 import Row from '../components/uikit/Row';
+import logo from '../logo.svg';
 
 const projects = [
-    { "name": "CTRL API", description: "Blockchain API" },
-    { "name": "Something", description: "Does stuff" },
-    { "name": "Something", description: "Does stuff" },
-    { "name": "Something", description: "Does stuff" },
-    { "name": "Something", description: "Does stuff" },
-    { "name": "Something", description: "Does stuff" }
+    { "name": "CTRL API", description: "Blockchain API", img: logo, detail: "Does A. Does B. Solves problem X." },
+    { "name": "Something", description: "Does stuff", img: logo, detail: "Does A. Does B. Solves problem X." },
+    { "name": "Something", description: "Does stuff", img: logo, detail: "Does A. Does B. Solves problem X." },
+    { "name": "Something", description: "Does stuff", img: logo, detail: "Does A. Does B. Solves problem X." },
+    { "name": "Something", description: "Does stuff", img: logo, detail: "Does A. Does B. Solves problem X." },
+    { "name": "Neat Test", description: "Very neat.", img: logo, detail: "Does A. Does B. Solves problem X." }
+    // TODO: add detailed_description field
 ];
 
 function project(n, setProjectIdx) {
     return (
-        <div uk-toggle="target: #project-id" className="uk-card uk-card-body uk-link-toggle" onClick={() => { setProjectIdx(n) }}>
-            <h3 className="uk-card-title uk-link-heading"><a href={`#?p=${n}`}>{projects[n].name}</a></h3>
+        <div key={n} uk-toggle="target: #project-id" className="uk-card uk-card-body uk-link-toggle"
+            onClick={() => { setProjectIdx(n) }}>
+            <h3 className="uk-card-title uk-link-heading">
+                <a href={`#?p=${n}`}>{projects[n].name}</a>
+            </h3>
             <p>{projects[n].description}</p>
+            <img src={projects[n].img} className="App-logo" alt="" />
         </div>
     );
 }
@@ -36,7 +42,13 @@ export default (props) => {
             <div id="project-id" uk-modal="true">
                 <div className="uk-modal-dialog uk-modal-body">
                     <h2 className="uk-modal-title">{projects[projectIdx].name}</h2>
-                    <button className="uk-modal-close" type="button">Click</button>
+                    <Row cols={2}>
+                        <img src={projects[projectIdx].img} alt="" />
+                        <div>
+                            <p>{projects[projectIdx].detail}</p>
+                            <button className="uk-modal-close" type="button">OK</button>
+                        </div>
+                    </Row>
                 </div>
             </div>
         </div>

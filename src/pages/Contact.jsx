8e76a58a8722dc6174593b35dee9548cda97c827
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-10-06 06:03:43 -0700

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,11 +4,19 @@ import Row from '../components/uikit/Row';
 export default (props) => {
     return (
         <div className="uk-container uk-padding">
-            <Row cols={3}>
-                <p>oh fuck</p>
-                <p>oh shit</p>
-                <p>oh heck</p>
-            </Row>
+            <div className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid="true">
+                <div className="uk-card-media-left uk-cover-container">
+                    <img src="/img/ctrl.png" alt="" uk-cover="true" />
+                    <canvas width="600" height="400"></canvas>
+                </div>
+                <div>
+                    <div className="uk-card-body">
+                        <h3 className="uk-card-title">Get In Touch</h3>
+                        <p>Questions, comments, concerns? Need something?</p>
+                        <p>Send us an email: <a href="mailto:brock@smedleyconsulting.com">brock@smedleyconsulting.com</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };

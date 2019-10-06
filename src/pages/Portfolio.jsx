37f4236dac66fd3f6184bf904515f47ffa3cca06
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-10-06 05:35:02 -0700

    add pictures
    + tech details per project

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,34 +4,40 @@ import logo from '../logo.svg';
 
 const projects = [
     {
-        "name": "24HourStatus", description: "Project Management Website", img: "/img/24hs.png",
+        "name": "24HourStatus", description: "Project management website", img: "/img/24hs.png",
         detail: "Let your clients access & update projects without them having to register an account. Created for IRC RES LLC.",
-        href: "https://24hourstatus.com"
+        href: "https://24hourstatus.com",
+        tech: ["react", "bootstrap", "next.js", "express.js", "stripe.js", "docker", "mongodb", "aws-s3", "nginx", "js"]
     },
     {
-        "name": "CTRL API", description: "REST-based Blockchain API", img: logo,
+        "name": "CTRL API", description: "REST-based blockchain API", img: "/img/ctrl.png",
         detail: "CTRL API is a REST API that acts as a CRUD database on Ethereum and IPFS. Created for VaaS Technologies, Inc.",
-        href: "https://github.com/LedgerCTRL/ctrl-api-python"
+        href: "https://github.com/LedgerCTRL/ctrl-api-python",
+        tech: ["flask", "python3", "docker", "mongodb", "redis", "ethereum", "ipfs", "parity", "python"]
     },
     {
-        "name": "YelpCSV", description: "React-based Yelp Scraper", img: logo,
+        "name": "YelpCSV", description: "React-based Yelp scraper", img: "/img/yelpcsv.png",
         detail: "Fetches search results from Yelp and converts results into a CSV spreadsheet. Created for myofascialreleaser.com",
-        href: "https://github.com/BrockSmedley/YelpCSV"
+        href: "https://github.com/BrockSmedley/YelpCSV",
+        tech: ["react", "materialize", "js", "docker"]
     },
     {
-        "name": "CWBY Coin", description: "Experimental Crypto Exchange & Marketplace", img: logo,
+        "name": "CWBY Coin", description: "Experimental crypto exchange & marketplace", img: "/img/cwby.png",
         detail: "Purchase CWBY coins and spend them on merchandise in one site.",
-        href: "http://cwby.biz/"
+        href: "http://cwby.biz/",
+        tech: ["flask", "python3", "web3", "ethereum", "erc20", "stripe.js", "js"]
     },
     {
-        "name": "xfinite", description: "Network Monitoring/Reporting Tool", img: logo,
+        "name": "xfinite", description: "Network monitoring/reporting tool", img: "/img/xfinite.png",
         detail: "Monitors network speed and tweets at your provider when it's below a threshold.",
-        href: "https://github.com/BrockSmedley/xfinite"
+        href: "https://github.com/BrockSmedley/xfinite",
+        tech: ["python3", "twitter", "docker", "cron"]
     },
     {
         "name": "L8S Software Services", description: "This site.", img: logo,
         detail: "See the source code for the website you're viewing right now!",
-        href: "https://github.com/BrockSmedley/smedley-consulting-portfolio"
+        href: "https://github.com/BrockSmedley/smedley-consulting-portfolio",
+        tech: ["react", "uikit-css", "js", "heroku"]
     }
 ];
 
@@ -53,6 +59,12 @@ function project(n, setProjectIdx) {
                 <div className="uk-card-body uk-link-heading">
                     <h3 className="uk-card-title"><a href={`#?p=${n}`}>{projects[n].name}</a></h3>
                     <p>{projects[n].description}</p>
+
+                    <div className="uk-grid uk-flex-center uk-grid-margin" uk-grid="true" style={{ padding: 32 }}>
+                        {projects[n].tech.map((val, idx) => {
+                            return <span>{val}</span>;
+                        })}
+                    </div>
                 </div>
             </div>
         </div >

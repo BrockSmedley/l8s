4420d9452be95795caaa8fce58c73fb84934bc8a
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-10-06 00:37:09 -0700

    map list of projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import Row from '../components/uikit/Row';
 
+const projects = [
+    { "name": "CTRL API", description: "Blockchain API" },
+    { "name": "Something", description: "Does stuff" },
+    { "name": "Something", description: "Does stuff" },
+    { "name": "Something", description: "Does stuff" },
+    { "name": "Something", description: "Does stuff" },
+    { "name": "Something", description: "Does stuff" }
+];
+
 function project(n, setProjectIdx) {
     return (
         <div uk-toggle="target: #project-id" className="uk-card uk-card-body uk-link-toggle" onClick={() => { setProjectIdx(n) }}>
-            <h3 className="uk-card-title uk-link-heading"><a href={`#?p=${n}`}>Project {n}</a></h3>
-            <p>It does something.</p>
+            <h3 className="uk-card-title uk-link-heading"><a href={`#?p=${n}`}>{projects[n].name}</a></h3>
+            <p>{projects[n].description}</p>
         </div>
     );
 }
@@ -16,20 +25,17 @@ export default (props) => {
     return (
         <div className="uk-container">
             <Row cols={3}>
-                {project(1, setProjectIdx)}
-                {project(2, setProjectIdx)}
-                {project(3, setProjectIdx)}
-                {project(4, setProjectIdx)}
-                {project(5, setProjectIdx)}
-                {project(6, setProjectIdx)}
+                {
+                    projects.map((val, idx) => {
+                        return project(idx, setProjectIdx);
+                    })
+                }
             </Row>
-            {/* <!-- This is a button toggling the modal --> */}
-            <button uk-toggle="target: #project-id" type="button">Wow</button>
 
             {/* <!-- This is the modal --> */}
             <div id="project-id" uk-modal="true">
                 <div className="uk-modal-dialog uk-modal-body">
-                    <h2 className="uk-modal-title">Modal {projectIdx}</h2>
+                    <h2 className="uk-modal-title">{projects[projectIdx].name}</h2>
                     <button className="uk-modal-close" type="button">Click</button>
                 </div>
             </div>

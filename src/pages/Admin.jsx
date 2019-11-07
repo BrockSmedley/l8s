032921152050bc6d9f6659cfeac6f26093838f36
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-11-06 21:56:19 -0800

    client foundation for DB storage

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,47 +1,72 @@
 import React, { Component } from 'react';
-// import Dante from 'Dante2';
+import PropTypes from 'prop-types';
+import RichTextEditor from 'react-rte';
+import uuid from 'uuidv4';
 
-// export default (props) => {
-//     const [content, setContent] = useState(null);
+class Editor extends Component {
+    static propTypes = {
+        onChange: PropTypes.func,
+        onChangeTitle: PropTypes.func,
+        onBlur: PropTypes.func
+    };
+    id = uuid();
 
-//     return (
-//         // <div className="uk-container uk-padding">
+    state = {
+        value: RichTextEditor.createEmptyValue(),
+        title: ""
+    }
 
-//         // </div>
-//         <Dante
-//             content={content}
-//             onChange={editor => { setContent(editor.emitSerializedOutput()); console.log(editor.emitSerializedOutput()); }}
-//         />
-//     )
-// }
+    onChange = (value) => {
+        this.setState({ value });
+        if (this.props.onChange) {
+            // Send the changes up to the parent component as an HTML string.
+            // This is here to demonstrate using `.toString()` but in a real app it
+            // would be better to avoid generating a string on each change.
+            this.props.onChange(
+                value.toString('html')
+            );
+        }
+        console.log(value);
+    };
 
-// class Admin extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             content: null
-//         };
-//         this.setContent = this.setContent.bind(this);
-//     }
-//     setContent(content) {
-//         this.setState({ content });
-//     }
-//     render() {
-//         return (
-//             <Dante
-//                 content={this.state.content}
-//                 onChange={editor => { this.setContent(editor.emitSerializedOutput()); console.log(editor.emitSerializedOutput()); }}
-//             />
-//         )
-//     }
-// }
+    onChangeTitle = (e) => {
+        this.setState({ title: e.target.value });
+    };
+    trimValue(value) {
+        return value.replace(/[\r\n]+/gm, "")
+    }
+    onBlur = () => {
+        // save state to DB if there's a title and some content
+        if (this.state.title && this.state.value && (this.trimValue(this.state.value.toString('markdown'))).toString().length > 1) {
+            console.log(
+                (this.trimValue(this.state.value.toString('markdown'))).toString().length
+            );
+            console.log(`saving to DB ${this.id}`);
+        }
+        else {
+            console.log("empty... not saving.");
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <input className="uk-input" type="text" id="title" name="title" placeholder="Title" onChange={this.onChangeTitle} value={this.state.title}></input>
+                <RichTextEditor
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    onBlur={this.onBlur}
+                />
+            </div>
+        );
+    }
+}
 
 export default (props) => {
     return (
         <div style={{ margin: 200, padding: 200 }}>
             testing...
+            <Editor />
         </div>
     );
 }
-
-// export default Admin;

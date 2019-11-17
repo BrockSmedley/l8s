9d4b08f085fefd311656546941df5c99a6e87193
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-11-16 19:04:44 -0800

    squish compiler warnings

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
-import UIkit from 'uikit';
-import axios from 'axios';
+import React from 'react';
+const email = process.env.REACT_APP_EMAIL;
 
 export default (props) => {
-    var [message, setMessage] = useState("");
-    var [email, setEmail] = useState("");
     return (
         <div className="uk-container uk-padding">
             <div className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid="true">
@@ -15,7 +12,7 @@ export default (props) => {
                 <div>
                     <div className="uk-card-body">
                         <h3 className="uk-card-title">Let's talk business.</h3>
-                        <p>Send us an email: <a href="mailto:brock@smedleyconsulting.com">brock@smedleyconsulting.com</a></p>
+                        <p>Send us an email: <a href={`mailto:${email}`}>{email}</a></p>
                     </div>
                 </div>
             </div>

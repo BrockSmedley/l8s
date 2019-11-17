a58e0eaf6c3e1878091c7cc57456cdacab721dfc
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-11-16 18:49:41 -0800

    remove nodemailer

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 console.log(process.env);
@@ -16,36 +15,6 @@ app.use(bodyParser.json());
 
 app.get("/test", (req, res) => {
     res.send("happy easter");
-});
-
-app.post("/contact_send", async (req, res) => {
-    let email = req.body.email;
-    let message = req.body.message;
-    console.log(email, message);
-
-    let testAccount = await nodemailer.createTestAccount();
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'oauth2',
-            user: process.env.EMAIL_USER,
-            clientId: process.env.EMAIL_CLIENT_ID,
-            clientSecret: process.env.EMAIL_CLIENT_SECRET,
-            refreshToken: process.env.EMAIL_REFRESH_TOKEN,
-            accessToken: process.env.EMAIL_ACCESS_TOKEN
-        }
-    });
-    let info = await transporter.sendMail({
-        from: `"L8S Support" <${process.env.EMAIL_USER}>`,
-        to: 'brock@smedleyconsulting.com',
-        subject: `L8S Contact Form - ${email}`,
-        text: `from ${email}
-        
-        ${message}`
-    });
-    console.log("message sent: %s", info.messageId);
-
-    res.send("OK");
 });
 
 // letsencrypt challenge

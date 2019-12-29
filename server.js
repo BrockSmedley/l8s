fe115a77406733ba65a498dc8104f27bf1bf680d
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-12-29 13:07:36 -0800

    install & enable cors
    (all requests for now)

--- server.js
+++ server.js
@@ -3,33 +3,41 @@ const bodyParser = require("body-parser");
 const path = require("path");
 var mongoose = require('mongoose');
 var blogRouter = require('./src/api/routes/blog');
-require('dotenv').config();
+var cors = require('cors');
 
+// .env
 require('dotenv').config();
 console.log(process.env);
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true
 });
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongoose connection error'));
 db.once('open', () => {
     console.log("Mongoose connected to MongoDB Atlas.");
 });
 
+// express
 const app = express();
 
+// enable CORS (all origins -- TODO: only enable on public-facing endpoints; restrict POST access to /blog)
+app.use(cors());
+
+// serve static files thru express
 app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+// healthcheck-ish
 app.get("/test", (req, res) => {
     res.send("happy easter");
 });
 
+// admin (TODO: use this in an auth middleware)
 app.post("/login", (req, res) => {
     let password = req.body.password;
     if (password == process.env.ADMIN_PASS) {

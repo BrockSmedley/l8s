937d69b66e877e22efcb674c5a996ece8efef8ec
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-12-29 13:29:11 -0800

    Update server.js

--- server.js
+++ server.js
@@ -23,11 +23,12 @@ db.once('open', () => {
 const app = express();
 
 // enable CORS (all origins -- TODO: only enable on public-facing endpoints; restrict POST access to /blog)
-var corsOptions = {
-    origin: 'https://www.l8s.co',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+// var corsOptions = {
+//     origin: 'https://www.l8s.co',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+// app.use(cors(corsOptions));
+app.use(cors());
 
 // serve static files thru express
 app.use(express.static(path.join(__dirname, "build")));
